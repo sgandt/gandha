911f02282d017d1bb8ec9e261a82cb33f0cb6424
sgandt <35653041+sgandt@users.noreply.github.com>
2018-02-09 16:02:01 +0530

    Create kumar.c

---
+++ kumar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,b;
+    scanf("%d%d",&m,&n);
+    for(i=a;i<b;i++)
+    {
+       if(i%2==0)
+       {
+           printf("%d\t",i);
+       }
+    }
+}
