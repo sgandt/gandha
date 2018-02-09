36a75ed2e28f9b422891e3aac3323495ee0e91b5
sgandt <35653041+sgandt@users.noreply.github.com>
2018-02-09 15:47:06 +0530

    Create gandha.c

---
+++ gandha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int b,m,count=0;
+scanf("%d",&a);
+for(n=2;m<b;n++)
+{
+if(a%m==0)
+count++;
+}
+if(count==0)
+printf("prime number\m");
+else
+printf("not a prime number\m");
+}
